13ad6ecdbdae8ea4ce12e9318de5e4afa19f93a9
Sydney <sydlyn626@gmail.com>
2024-08-03 20:49:54 -0700

    basic starting point

---
+++ app/(app)/_layout.tsx
@@ -0,0 +1,8 @@
+import { Stack } from 'expo-router';
+import React from 'react';
+
+export default function TabLayout() {
+  return (
+    <Stack screenOptions={{headerShown: false}} />
+  );
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,10 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { StatusBar } from 'react-native';
+
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
@@ -27,9 +28,10 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={DefaultTheme}>
+      <StatusBar barStyle='light-content'/>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(app)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
     </ThemeProvider>
