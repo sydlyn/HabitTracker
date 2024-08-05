54db097036f2b534bc17b954858e2dd25c4fc564
Sydney <sydlyn626@gmail.com>
2024-08-05 02:05:36 -0700

    fix game bug: square random placement border spacing

--- app/(tabs)/game.tsx
+++ app/(tabs)/game.tsx
@@ -1,26 +1,35 @@
 import { useState, useEffect } from 'react';
-import { SafeAreaView, Text, Pressable, StyleSheet, DimensionValue } from 'react-native';
+import { SafeAreaView, View, Text, Pressable, StyleSheet, DimensionValue } from 'react-native';
 
 import { custColors } from '@/constants/Colors'
 
 export default function GameScreen() {
 	const [clickCounter, setClickCounter] = useState(0);
-	const [x, setX] = useState(Math.floor(Math.random() * 85) + '%');
-	const [y, setY] = useState(Math.floor(Math.random() * 90) + '%');
+	const [x, setX] = useState('50%');
+	const [y, setY] = useState('40%');
 
 	useEffect(() => {
-		setX(Math.floor(Math.random() * 85) + '%')
-		setY(Math.floor(Math.random() * 90) + '%')
+		if (clickCounter) {
+			setX(Math.floor(Math.random() * 100) + '%')
+			setY(Math.floor(Math.random() * 100) + '%')
+		}
 	}, [clickCounter]);
 
 	return (
 		<SafeAreaView style={styles.pageStyle}>
-			<Pressable 
-				onPress={() => setClickCounter(clickCounter + 1)}
-				style={[styles.movingSquare, {left: x as DimensionValue, top: y as DimensionValue, bottom: 10}]}
-			>
-				<Text style={styles.squareText}>{clickCounter}</Text>
-			</Pressable>
+			{/* header title at the start of the game */}
+			{clickCounter ? <></> 
+				: <Text style={styles.headerText}>Click the Square to Begin</Text>}
+
+			{/* safe space that the square to appear in */}
+			<View style={styles.safeSquareSpace}>
+				<Pressable 
+					onPress={() => setClickCounter(clickCounter + 1)}
+					style={[styles.movingSquare, {left: x as DimensionValue, top: y as DimensionValue}]}
+				>
+					<Text style={styles.squareText}>{clickCounter}</Text>
+				</Pressable>
+			</View>
 		</SafeAreaView>
 	)
 };
@@ -29,6 +38,21 @@ const styles = StyleSheet.create({
 	pageStyle: {
 		flex: 1,
 		backgroundColor: custColors.sageGreen
+	},
+	safeSquareSpace: {
+		flex: 1,
+		backgroundColor: custColors.sageGreen,
+		paddingRight: 60,
+		paddingBottom: 60,
+		paddingLeft: 10,
+		paddingTop: 10
+	},
+	headerText: {
+		color: 'white',
+		justifyContent: 'center',
+		textAlign: 'center',
+		fontSize: 20,
+		padding: 20
 	},
 	movingSquare: {
 		backgroundColor: 'white',
